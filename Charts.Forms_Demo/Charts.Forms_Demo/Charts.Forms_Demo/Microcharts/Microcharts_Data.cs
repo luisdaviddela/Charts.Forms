35ba4ba479b0b263051d41d1e25053fed86e139c
Luis David De la cruz <delacruz_luis@outlook.es>
2018-03-17 00:06:04 -0600

    All The source code with the implementation of Microcharts.Forms

---
+++ Charts.Forms_Demo/Charts.Forms_Demo/Charts.Forms_Demo/Microcharts/Microcharts_Data.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SkiaSharp;
+using Entry = Microcharts.Entry;
+namespace Charts.Forms_Demo
+{
+    public class Microcharts_Data
+    {
+        public List<Entry> GetChart()
+        {
+            List<Entry> data = new List<Entry>
+        {
+            new Entry(1563532)
+            {
+                Label = "01 Ene 16",
+                ValueLabel ="1563532",
+                Color = SKColor.Parse("#2E4053"),
+                TextColor = SKColor.Parse("#DF013A"),
+            },
+            new Entry(2919064)
+            {
+                Label = "02 Feb 16",
+                ValueLabel ="2919064",
+                Color = SKColor.Parse("#2E4053"),
+                TextColor = SKColor.Parse("#DF013A"),
+            },
+            new Entry(2922564)
+            {
+                Label = "03 Mar 16",
+                ValueLabel ="2922564",
+                Color = SKColor.Parse("#2E4053"),
+                TextColor = SKColor.Parse("#DF013A"),
+            },
+            new Entry(4495396)
+            {
+                Label = "04 Abr 16",
+                ValueLabel ="4495396",
+                Color = SKColor.Parse("#2E4053"),
+                TextColor = SKColor.Parse("#DF013A"),
+            },
+            new Entry(6086428)
+            {
+                Label = "05 May 16",
+                ValueLabel ="6086428",
+                Color = SKColor.Parse("#2E4053"),
+                TextColor = SKColor.Parse("#DF013A"),
+            },
+            new Entry(7646260)
+            {
+                Label = "06 Jun 16",
+                ValueLabel ="7646260",
+                Color = SKColor.Parse("#2E4053"),
+                TextColor = SKColor.Parse("#DF013A"),
+            },
+            new Entry(8526092)
+            {
+                Label = "07 Jul 16",
+                ValueLabel ="8526092",
+                Color = SKColor.Parse("#2E4053"),
+                TextColor = SKColor.Parse("#DF013A"),
+            },
+            new Entry(9905924)
+            {
+                Label = "08 Ago 16",
+                ValueLabel ="9905924",
+                Color = SKColor.Parse("#2E4053"),
+                TextColor = SKColor.Parse("#DF013A"),            },
+            new Entry(10923090)
+            {
+                Label = "09 Sep 16",
+                ValueLabel ="10923090",
+                Color = SKColor.Parse("#2E4053"),
+                TextColor = SKColor.Parse("#DF013A"),
+            },
+            new Entry(12052922)
+            {
+                Label = "10 Oct 16",
+                ValueLabel ="12052922",
+                Color = SKColor.Parse("#2E4053"),
+                TextColor = SKColor.Parse("#DF013A"),
+            },
+            new Entry(13052754)
+            {
+                Label = "11 Nov 16",
+                ValueLabel ="13052754",
+                Color = SKColor.Parse("#2E4053"),
+                TextColor = SKColor.Parse("#DF013A"),
+            },
+            new Entry(14052586)
+            {
+                Label = "12 Dic 16",
+                ValueLabel ="14052586",
+                Color = SKColor.Parse("#2E4053"),
+                TextColor = SKColor.Parse("#DF013A"),
+            },
+            new Entry(14088586)
+            {
+                Label = "01 Ene 17",
+                ValueLabel ="14088586",
+                Color = SKColor.Parse("#2E4053"),
+                TextColor = SKColor.Parse("#DF013A"),
+            },
+        };
+
+            return data;
+        }
+    }
+}
---
+++ Charts.Forms_Demo/Charts.Forms_Demo/Charts.Forms_Demo/Microcharts/Microcharts_View.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microcharts;
+
+namespace Charts.Forms_Demo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Microcharts_View : ContentPage
+	{
+        Microcharts_Data data = new Microcharts_Data();
+        public Microcharts_View ()
+		{
+			InitializeComponent ();
+            Chartedemo.Chart = new LineChart { Entries = data.GetChart() };
+            Chartedemo1.Chart = new DonutChart { Entries = data.GetChart() };
+            Chartedemo2.Chart = new PieChart { Entries = data.GetChart() };
+            Chartedemo3.Chart = new PointChart { Entries = data.GetChart() };
+            Chartedemo4.Chart = new RadarChart{ Entries = data.GetChart() };
+            Chartedemo5.Chart = new RadialGaugeChart { Entries = data.GetChart() };
+            Chartedemo6.Chart = new BarChart { Entries = data.GetChart() };
+        }
+	}
+}
