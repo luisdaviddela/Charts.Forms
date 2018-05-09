10d27573d01bb22535cd4095327fc3a88f23db6d
Luis David De la cruz <delacruz_luis@outlook.es>
2018-05-09 13:16:39 -0500

    Overlay chart

---
+++ Charts.Forms_Demo/Charts.Forms_Demo/Charts.Forms_Demo/Syncfusion/ViewModels/VM_PorcentajeContenidoNacional.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Charts.Forms_Demo
+{
+    class VM_PorcentajeContenidoNacional
+    {
+        public List<Person> Serie1 { get; set; }
+        public List<Person> Serie2 { get; set; }
+        public VM_PorcentajeContenidoNacional()
+        {
+            Serie1 = new List<Person>()
+            {
+                new Person { Name= "Luis", Height=10},
+                new Person { Name= "Reyna", Height=8}
+            };
+
+
+            Serie2 = new List<Person>()
+            {
+                new Person { Name= "Luis", Height=8},
+                new Person { Name= "Reyna", Height=7}
+            };
+        }
+    }
+}
