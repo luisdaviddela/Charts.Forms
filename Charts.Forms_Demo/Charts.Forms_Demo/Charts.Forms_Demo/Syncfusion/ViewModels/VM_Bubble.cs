ac60582ef286608bc1cc706caf38e84e61a865f1
Luis David De la cruz <delacruz_luis@outlook.es>
2018-05-11 10:24:54 -0500

    Bubbles sample

---
+++ Charts.Forms_Demo/Charts.Forms_Demo/Charts.Forms_Demo/Syncfusion/ViewModels/VM_Bubble.cs
@@ -0,0 +1,24 @@
+﻿using Syncfusion.SfChart.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Charts.Forms_Demo
+{
+    class VM_Bubble
+    {
+
+        public ObservableCollection<ChartDataPoint> Data { get; set; }
+        public VM_Bubble()
+        {
+            Data = new ObservableCollection<ChartDataPoint>
+            {
+                new ChartDataPoint(1,3,3),
+                new ChartDataPoint(1,3.5,3),
+                new ChartDataPoint(1,2.99,3),
+                new ChartDataPoint(1,4,3)
+            };
+        }
+    }
+}
